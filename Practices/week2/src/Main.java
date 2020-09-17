702a050bd64c618c19fc02afca3c718d83688c82
JeongHyeon Lee <sjeonghyeonz@gmail.com>
2020-09-17 10:31:07 +0900

    make singleton design-pattern

--- Practices/week2/src/Main.java
+++ Practices/week2/src/Main.java
@@ -2,7 +2,7 @@ import java.util.Optional;
 
 public class Main {
     public static void main(String[] args) {
-        TicketManager tm = new TicketManager();
+        TicketManager tm = TicketManager.getInstance();
         int serialNum;
         Optional<Ticket> t1 = tm.issue();
         serialNum = t1.map(Ticket::getSerialNum).orElse(-1);
@@ -10,7 +10,13 @@ public class Main {
         Optional<Ticket> t2 = tm.issue();
         serialNum = t2.map(Ticket::getSerialNum).orElse(-1);
         System.out.println(serialNum);
-        Optional<Ticket> t3 = tm.issue();
+
+        /*
+         * 기존의 방식으로는 새로운 TicketManager를 생성하여 이미 발행했던 serial number가 나타났지만,
+         * singleton design pattern을 사용함으로써 기존 인스턴스를 이용할 수 있게 되었다.
+         */
+        TicketManager tm2 = TicketManager.getInstance();
+        Optional<Ticket> t3 = tm2.issue();
         serialNum = t3.map(Ticket::getSerialNum).orElse(-1);
         System.out.println(serialNum);
     }
--- Practices/week2/src/TicketManager.java
+++ Practices/week2/src/TicketManager.java
@@ -1,10 +1,22 @@
 import java.util.Optional;
 
 public class TicketManager {
+    /* TicketManager를 static으로 만들어 하나의 공유 객체로 만든다. */
+    private static TicketManager instance = null;
     private int count;
     private final int LIMITS = 2;
-    public TicketManager() {
+    /* 생성자를 private으로 만들어 외부에서 함부로 생성하지 못하도록 한다. */
+    private TicketManager() {
         count = 0;
+    }
+
+    /*
+     * TicketManager를 method를 통해서만 생성하도록 한다.
+     * 이미 생성되어 있다면 기존 instance를 사용하기 위해 불러온다.
+     */
+    public static TicketManager getInstance() {
+        if (instance == null) instance = new TicketManager();
+        return instance;
     }
 
     public Optional<Ticket> issue() {
