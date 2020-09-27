5c0523921b27562108395db32121c1cf439d6403
JeongHyeon Lee <sjeonghyeonz@gmail.com>
2020-09-27 09:16:51 +0900

    JavaBeans Pattern

--- Practices/week4/src/Book.java
+++ Practices/week4/src/Book.java
@@ -6,39 +6,33 @@ public class Book {
     private int pages;
     private String category;
 
-    /* Telescoping Constructor Pattern : 인자가 많을수록 생성자도 늘어남 */
+    /* JavaBeans Pattern : setter method로 속성 값을 설정하여 가독성은 향상되나 immutable object를 만들 수 없음 */
     public Book(Long id, String isbn) {
         this.id = id;
         this.isbn = isbn;
     }
 
-    public Book(Long id, String isbn, String title) {
+    public void setId(Long id) {
         this.id = id;
+    }
+
+    public void setIsbn(String isbn) {
         this.isbn = isbn;
+    }
+
+    public void setTitle(String title) {
         this.title = title;
     }
 
-    public Book(Long id, String isbn, String title, String author) {
-        this.id = id;
-        this.isbn = isbn;
-        this.title = title;
+    public void setAuthor(String author) {
         this.author = author;
     }
 
-    public Book(Long id, String isbn, String title, String author, int pages) {
-        this.id = id;
-        this.isbn = isbn;
-        this.title = title;
-        this.author = author;
+    public void setPages(int pages) {
         this.pages = pages;
     }
 
-    public Book(Long id, String isbn, String title, String author, int pages, String category) {
-        this.id = id;
-        this.isbn = isbn;
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
+    public void setCategory(String category) {
         this.category = category;
     }
 }
