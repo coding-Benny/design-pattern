b93b11aac01652921b2b42633b5646f12c73f693
JeongHyeon Lee <sjeonghyeonz@gmail.com>
2020-09-27 10:26:49 +0900

    Lombok @Builder Annotation

--- Practices/week4/src/Book.java
+++ Practices/week4/src/Book.java
@@ -1,78 +1,16 @@
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NonNull;
+
+@Getter
+@Builder
 public class Book {
+    @NonNull
     private Long id; // 필수
+    @NonNull
     private String isbn; // 필수
     private String title;
     private String author;
     private int pages;
     private String category;
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getIsbn() {
-        return isbn;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public int getPages() {
-        return pages;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public static class BookBuilder {
-        private Long id; // 필수
-        private String isbn; // 필수
-        private String title;
-        private String author;
-        private int pages;
-        private String category;
-
-        /* Builder Pattern : 가독성이 좋고 immutable 객체를 생성할 수 있으나 코드 양이 상당히 증가함 */
-        public BookBuilder(Long id, String isbn) {
-            this.id = id;
-            this.isbn = isbn;
-        }
-
-        public BookBuilder title(String title) {
-            this.title = title;
-            return this;
-        }
-
-        public BookBuilder author(String author) {
-            this.author = author;
-            return this;
-        }
-
-        public BookBuilder pages(int pages) {
-            this.pages = pages;
-            return this;
-        }
-
-        public BookBuilder category(String category) {
-            this.category = category;
-            return this;
-        }
-
-        public Book build() {
-            Book book = new Book();
-            book.id = this.id;
-            book.isbn = this.isbn;
-            book.author = this.author;
-            book.title = this.title;
-            book.pages = this.pages;
-            book.category = this.category;
-            return book;
-        }
-    }
 }
--- Practices/week4/src/Main.java
+++ Practices/week4/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        Book book1 = new Book.BookBuilder(1L, "isbn1234").author("Insang Chung").build();
-        Book book2 = new Book.BookBuilder(2L, "isbn2345").pages(360).author("Insang Chung").build(); // 인자의 순서 중요하지 않음
+        Book book1 = new Book.BookBuilder().id(1L).isbn("isbn1234").author("Insang Chung").build();
+        Book book2 = new Book.BookBuilder().id(2L).isbn("isbn2345").pages(360).author("Insang Chung").build(); // 인자의 순서 중요하지 않음
         System.out.println(book2.getAuthor());
     }
 }
