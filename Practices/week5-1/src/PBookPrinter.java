d7e9a42067fc638b78f133041cdfaa69122301e9
JeongHyeon Lee <sjeonghyeonz@gmail.com>
2020-10-03 00:26:29 +0900

    Design not satisfying OCP

--- Practices/week5-1/src/PBookPrinter.java
+++ Practices/week5-1/src/PBookPrinter.java
@@ -1,3 +1,5 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
@@ -14,35 +16,50 @@ public class PBookPrinter {
         this.book = book;
     }
 
-    public void printXml() {
-        try {
-            DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
-            Document doc = docBuilder.newDocument();
-            doc.setXmlStandalone(true);
+    public void print(PBookFormat f) {
+        Set keySet = book.getKeySet();
+        if (f == PBookFormat.XML) {
+            try {
+                DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+                DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+                Document doc = docBuilder.newDocument();
+                doc.setXmlStandalone(true);
 
-            Element pbook = doc.createElement("PhoneBook");
-            doc.appendChild(pbook);
-            Set keySet = book.getKeySet();
+                Element pbook = doc.createElement("PhoneBook");
+                doc.appendChild(pbook);
+                Iterator iterator = keySet.iterator();
+                while (iterator.hasNext()) {
+                    Element entry = doc.createElement("entry");
+                    pbook.appendChild(entry);
+                    Element name = doc.createElement("name");
+                    String nameKey = (String) iterator.next();
+                    name.appendChild(doc.createTextNode(nameKey));
+                    entry.appendChild(name);
+                    String value = book.getNumber(nameKey);
+                    String[] pnumbers = value.split(";");
+
+                    for (int i = 0; i < pnumbers.length; i++) {
+                        Element number = doc.createElement("number");
+                        number.appendChild(doc.createTextNode(pnumbers[i]));
+                        entry.appendChild(number);
+                    }
+                }
+            } catch (ParserConfigurationException e) {
+                e.printStackTrace();
+            }
+        }
+        else {
+            JSONObject jsonObject = new JSONObject();
+            JSONObject bookJson = new JSONObject();
+            JSONArray entries = new JSONArray();
             Iterator iterator = keySet.iterator();
             while (iterator.hasNext()) {
-                Element entry = doc.createElement("entry");
-                pbook.appendChild(entry);
-                Element name = doc.createElement("name");
+                JSONObject entry = new JSONObject();
                 String nameKey = (String) iterator.next();
-                name.appendChild(doc.createTextNode(nameKey));
-                entry.appendChild(name);
+                entry.put("name", nameKey);
                 String value = book.getNumber(nameKey);
                 String[] pnumbers = value.split(";");
-
-                for (int i = 0; i < pnumbers.length; i++) {
-                    Element number = doc.createElement("number");
-                    number.appendChild(doc.createTextNode(pnumbers[i]));
-                    entry.appendChild(number);
-                }
             }
-        } catch (ParserConfigurationException e) {
-            e.printStackTrace();
         }
     }
 }
